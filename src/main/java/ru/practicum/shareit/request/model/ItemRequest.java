7018073a90cbf9a9771dbcb0d9508f77e9a3bcc2
Radmir Nurgaliev <nradmir@gmail.com>
2026-09-17 21:32:27 +0300

    feat: подготовлена таблица, модель данных и JPA-репозиторий для запросов вещей

---
+++ src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
@@ -0,0 +1,37 @@
+package ru.practicum.shareit.request.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import ru.practicum.shareit.user.model.User;
+
+
+/**
+ * Модель данных для сущности запроса вещей
+ */
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "requests")
+public class ItemRequest {
+    /**
+     * Идентификатор бронирования
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    /**
+     * Описание запроса
+     */
+    private String description;
+    /**
+     * Пользователь создавший запрос вещи
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "requestor_id")
+    @ToString.Exclude
+    private User user;
+}
