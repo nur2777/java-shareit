42c4a0233d62144d4e419e8b8107865055e2add2
Radmir Nurgaliev <nradmir@gmail.com>
2026-09-24 07:01:37 +0300

    fix: исправлена ошибка checkstyle

--- server/src/main/java/ru/practicum/shareit/booking/service/BookingService.java
+++ server/src/main/java/ru/practicum/shareit/booking/service/BookingService.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.booking.service;
 
-import ru.practicum.shareit.booking.StateEnum;
 import ru.practicum.shareit.booking.dto.BookingResponseDTO;
 import ru.practicum.shareit.booking.dto.BookingRequestDTO;
 
